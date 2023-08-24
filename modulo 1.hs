a209198cd84948d4b95cbb6ec42846965962a84d
hpcat1010 <132513839+hpcat1010@users.noreply.github.com>
2023-08-24 09:49:12 -0300

    Add files via upload

---
+++ modulo 1.hs
@@ -0,0 +1,13 @@
+
+module Point ( Point, newP, difP)
+   where
+
+data Point = Poi Int Int deriving (Eq, Show)
+
+newP :: Int -> Int -> Point
+newP x xs = Poi x xs
+
+
+difP :: Point -> Point -> Float
+difP (Poi x1 y1)(Poi x2 y2) =  sqrt( fromIntegral((x1 - x2) ^ 2 + (y1 - y2) ^ 2))
+
